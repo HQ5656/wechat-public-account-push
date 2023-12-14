ddd7660a4eba64b6773922213c30d369cd0c45cd
HQ5656 <67215641+HQ5656@users.noreply.github.com>
2023-12-14 21:31:35 +0800

    Update index.cjs
    test

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxe9bc2a653ebe9d6c',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'oVYYt6yKOPKDB7Wm25DXkNJVlF5w',
 
   PROVINCE: '广东',
   CITY: '惠州',
