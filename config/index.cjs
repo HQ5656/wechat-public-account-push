ee014e5a0b34e3d95dd313a6eb293dc8e7794ba3
HQ5656 <67215641+HQ5656@users.noreply.github.com>
2023-12-16 16:21:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '3b4f2356f50c884bc3fc04b87ac7427a',
+      id: 'oVYYt6yKOPKDB7Wm25DXkNJVlF5w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KRD_LtqWtrhbK5VH2xuf94bTnK_kfCDRe9T4TQNHjVg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'kgZ064bDzYqmREXjQZDkFB6nnS95iWSfVxfq-DTpuy4',
+  CALLBACK_TEMPLATE_ID: 'KRD_LtqWtrhbK5VH2xuf94bTnK_kfCDRe9T4TQNHjVg',
 
   CALLBACK_USERS: [
     {
