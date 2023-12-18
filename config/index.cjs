81c58f661f076800b3056322dca25fc741eb7bf7
HQ5656 <67215641+HQ5656@users.noreply.github.com>
2023-12-18 09:26:30 +0800

    Update index.cjs
    1

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oVYYt6yKOPKDB7Wm25DXkNJVlF5w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'stiPdDkOkxU1ZE5xDfsZ3ah0pot6J0_koNJJMLFh_NY',
+      useTemplateId: 'O9efdPyMFus3j3BQCaicmFeWhgpAgFZAGxA4tAtjrzE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -31,12 +31,8 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '宝宝', year: '1995', date: '11-05',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2023', date: '07-16',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -44,37 +40,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-07-16' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-     {
-      // 想要发送的人的名字
-      name: '宝宝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oVYYt681QoQmgKCzbNvNjTDl9owE',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'stiPdDkOkxU1ZE5xDfsZ3ah0pot6J0_koNJJMLFh_NY',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝宝', year: '1995', date: '11-05',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-07-16' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
